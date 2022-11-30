aee8f0fb3b38aacdce2b709d85a7930bee267e4e
Raulj06 <5015@holbertonschool.com>
2022-11-30 21:22:12 +0000

    web app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Script that starts a Flask Web Application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def startup():
+	return "Hello HBNB!"
+
+if __name__=='__main__':
+	app.run(host='0.0.0.0')
