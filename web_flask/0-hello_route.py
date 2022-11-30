0a86fe8e60831f17d0e8d1826f4a6370c67a0035
Raulj06 <5015@holbertonschool.com>
2022-11-30 21:56:58 +0000

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def startup():
-	return "Hello HBNB!"
+def holberton_school():
+	return 'Hello HBNB!'
 
 if __name__=='__main__':
-	app.run(host="0.0.0.0", port=5000)
+	app.run(host='0.0.0.0', port=5000, debug=True)
