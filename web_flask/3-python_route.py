afe4c9fdf38af4e020dfd58a653f83d15584f8fa
Raulj06 <5015@holbertonschool.com>
2022-12-05 03:48:27 +0000

    update typo

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-""" Script that starts a Flask Web Application """
+
+
+"""start the Flask Web application by"""
 
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def startup():
-    """ Displays message on / route """
+def hello_HBNB():
+    """return a string from route"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb_route():
-    """ Displays message on /hbnb route """
+def hbnb():
+    """return a string from hbnb route"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """ Displays message on /c/ route """
-    return "C " + text.replace("_", " "))
+def display_c(text):
+    return "C " + text.replace("_", " ")
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
-    """ Display message on /python/ route"""
-    return 'Python {}'.format(text.replace("_", " "))
+def display_python(text='is cool'):
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
